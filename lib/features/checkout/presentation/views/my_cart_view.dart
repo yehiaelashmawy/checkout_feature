0f79da90144d71b817103eac033d2efaf9cee18f
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-09-18 11:59:59 +0300

    Create AppBar

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,6 @@
 import 'package:checkout_payment/core/utils/style.dart';
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -8,9 +9,12 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Align(
-          alignment: Alignment.center,
-          child: Text('My Cart', style: Style.style25),
+        leading: Center(child: SvgPicture.asset('assets/image/arrow.svg')),
+        centerTitle: true,
+        title: Text(
+          'My Cart',
+          textAlign: TextAlign.center,
+          style: Style.style25,
         ),
       ),
     );
