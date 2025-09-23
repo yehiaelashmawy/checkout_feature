855ea0b9b237889bf29b86b4eac2c32f55f6aa14
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-09-23 14:25:24 +0300

    create stripe service & Api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+  Future<Response> post({
+    required body,
+    required String url,
+    required String token,
+    String? contentype,
+  }) async {
+    var response = await dio.post(
+      url,
+      data: body,
+      options: Options(
+        contentType: contentype,
+        headers: {'Authorization': 'Bearer $token'},
+      ),
+    );
+
+    return response;
+  }
+}
---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,8 @@
+import 'package:checkout_payment/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_payment/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
+class StripeService {
+  // PaymentIntentModel createpaymentintent(
+  //   PaymentIntentInputModel paymentIntentInputModel,
+  // ) {}
+}
