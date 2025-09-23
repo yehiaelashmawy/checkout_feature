8401350efef2022292083c973a7359353e300e19
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-09-23 15:11:20 +0300

    Implement init payment sheet

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -2,6 +2,7 @@ import 'package:checkout_payment/core/utils/api_keys.dart';
 import 'package:checkout_payment/core/utils/api_service.dart';
 import 'package:checkout_payment/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:checkout_payment/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class StripeService {
   final ApiService apiService = ApiService();
@@ -17,5 +18,14 @@ class StripeService {
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
 
     return paymentIntentModel;
+  }
+
+  Future initpaymentsheet({required String paymentInitClientSecret}) async {
+    Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentInitClientSecret,
+        merchantDisplayName: 'Yehia Elashmawy',
+      ),
+    );
   }
 }
